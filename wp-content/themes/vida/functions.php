a168547aec9df8b3c605bc52ed5ce62058800901
fedesouto <federicogsouto@gmail.com>
2023-10-26 14:43:38 -0300

    shop searchform and filters

--- wp-content/themes/vida/functions.php
+++ wp-content/themes/vida/functions.php
@@ -2,26 +2,30 @@
 
 
 
-function vida_enqueue_styles() {
-	$parenthandle = 'astra-style'; 
+function vida_enqueue_styles()
+{
+	$parenthandle = 'astra-style';
 	$theme        = wp_get_theme();
-	wp_enqueue_style( $parenthandle,
+	wp_enqueue_style(
+		$parenthandle,
 		get_template_directory_uri() . '/style.css',
 		array(),  // If the parent theme code has a dependency, copy it to here.
-		$theme->parent()->get( 'Version' )
+		$theme->parent()->get('Version')
 	);
-	wp_enqueue_style( 'vida-style',
+	wp_enqueue_style(
+		'vida-style',
 		get_stylesheet_uri(),
-		array( $parenthandle ),
-		$theme->get( 'Version' ) // This only works if you have Version defined in the style header.
+		array($parenthandle),
+		$theme->get('Version') // This only works if you have Version defined in the style header.
 	);
 }
 
-add_action( 'wp_enqueue_scripts', 'vida_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'vida_enqueue_styles');
 
 use Tribe\Events\Models\Post_Types\Event;
 
-function vida_filter_events_by_attribute($post) {
+function vida_filter_events_by_attribute($post)
+{
 	//var_dump(get_object_vars($post));
 
 	/*foreach(get_object_vars($post) as $prop){
@@ -29,8 +33,8 @@ function vida_filter_events_by_attribute($post) {
 	}*/
 	//$event = Event::from_post($post);
 	//var_dump(get_object_vars($post));
-	
-	
+
+
 	//echo "<script>console.log('Event " . $post->start_date. "' );</script>";
 	//echo "<script>console.log('Event " . $post->venue. "' );</script>";
 	/*echo "<script>console.log('category " . tribe_get_venue($post->ID). "' );</script>";
@@ -39,61 +43,86 @@ function vida_filter_events_by_attribute($post) {
 	}*/
 	//echo "<script>console.log('Category " . get_the_category($post->ID). "' );</script>";
 	//var_dump(get_the_category($post->ID));
-	
+
 	return $post;
 }
 
-add_filter('tribe_get_event', 'vida_filter_events_by_attribute'); 
+add_filter('tribe_get_event', 'vida_filter_events_by_attribute');
 
-function vida_add_woocommerce_search_bar() {
+
+/**
+ * 
+ * Shop functions.
+ * 
+ */
+
+
+function vida_add_woocommerce_search_bar()
+{
 	wc_get_template_part('product', 'searchform');
 }
 
-remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count',20);
-remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering',30);
+remove_action('woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+remove_action('woocommerce_before_shop_loop', 'woocommerce_catalog_ordering', 30);
 add_action('woocommerce_before_shop_loop', 'vida_add_woocommerce_search_bar', 2);
 
-function vida_add_single_product_description() {
-    get_template_part("template-parts/vida", "single-product-description");
+function vida_add_single_product_description()
+{
+	get_template_part("template-parts/vida", "single-product-description");
 }
 
 remove_action('woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10);
 add_action('woocommerce_after_single_product_summary', 'vida_add_single_product_description', 10);
 
-function filter_products() {
+function filter_products()
+{
 	$color = $_POST['color'];
 	$size = $_POST['size'];
-  
+
 	$tax_query = [];
 
-	if($color !== "all") {
-		$tax_query[] = array('taxonomy' => 'pa_color',
-		'field' => 'slug',
-		'terms' => $color);
+	if ($color !== "all") {
+		$tax_query[] = array(
+			'taxonomy' => 'pa_color',
+			'field' => 'slug',
+			'terms' => $color
+		);
 	}
-	if($size !== "all") {
-		$tax_query[] = array('taxonomy' => 'pa_size',
-		'field' => 'slug',
-		'terms' => $size);
+	if ($size !== "all") {
+		$tax_query[] = array(
+			'taxonomy' => 'pa_size',
+			'field' => 'slug',
+			'terms' => $size
+		);
 	}
 
 	$ajaxposts = new WP_Query([
-	  'post_type' => 'product',
-	  'posts_per_page' => -1,
-	  'tax_query' => $tax_query
+		'post_type' => 'product',
+		'posts_per_page' => -1,
+		'tax_query' => $tax_query
 	]);
 	$response = '';
-  
-	if($ajaxposts->have_posts()) {
-	  while($ajaxposts->have_posts()) : $ajaxposts->the_post();
-		$response .= wc_get_template_part('content', 'product');
-	  endwhile;
+
+	if ($ajaxposts->have_posts()) {
+		while ($ajaxposts->have_posts()) : $ajaxposts->the_post();
+			$response .= wc_get_template_part('content', 'product');
+		endwhile;
 	} else {
-	  $response = 'empty';
+		$response = do_action('woocommerce_no_products_found');
 	}
 	echo $response;
 	wp_reset_postdata();
 	exit;
-  }
-  add_action('wp_ajax_filter_products', 'filter_products');
-  add_action('wp_ajax_nopriv_filter_products', 'filter_products');
+}
+add_action('wp_ajax_filter_products', 'filter_products');
+add_action('wp_ajax_nopriv_filter_products', 'filter_products');
+
+remove_action('woocommerce_after_shop_loop', 'woocommerce_pagination', 10);
+
+add_filter('loop_shop_per_page', 'vida_remove_pagination', 20);
+
+function vida_remove_pagination($cols)
+{
+	$cols = 90;
+	return $cols;
+}
--- wp-content/themes/vida/woocommerce/product-searchform.php
+++ wp-content/themes/vida/woocommerce/product-searchform.php
@@ -23,8 +23,6 @@ if (!defined('ABSPATH')) {
 ?>
 
 <script>
-	const products = document.querySelectorAll('.products')[0];
-	
 	function filterProducts(event) {
 		event.preventDefault();
 
@@ -35,7 +33,6 @@ if (!defined('ABSPATH')) {
 			color: colorFilter.value,
 			size: sizeFilter.value
 		}
-		console.log(query)
 		jQuery.ajax({
 			type: 'POST',
 			url: '/argygestionable/wp-admin/admin-ajax.php',
@@ -45,13 +42,26 @@ if (!defined('ABSPATH')) {
 				color: query.color,
 				size: query.size
 			},
+			beforeSend: function() {
+				jQuery('.products').html('<div class="vida-loader"></div>')
+			},
 			success: function(res) {
+
 				jQuery('.products').html(res);
 			},
 			error: function(err) {
 				console.log(err);
 			}
 		});
+	}
+
+	function resetFilters(event) {
+		const colorFilter = document.querySelector('#color_filter')
+		const sizeFilter = document.querySelector('#size_filter')
+		event.preventDefault();
+		colorFilter.value = "all";
+		sizeFilter.value = "all";
+		filterProducts(event);
 	}
 </script>
 
@@ -66,20 +76,25 @@ if (!defined('ABSPATH')) {
 		<input type="search" id="woocommerce-product-search-field-<?php echo isset($index) ? absint($index) : 0; ?>" class="search-field vida-product-search-field" placeholder="<?php echo esc_attr__('Search products&hellip;', 'woocommerce'); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 		<input type="hidden" name="post_type" value="product" />
 	</form>
-	<form onsubmit="filterProducts(event)">
-		<select name="color" id="color_filter">
+	<form onsubmit="filterProducts(event)" onreset="resetFilters(event)" class="vida-product-filter-container">
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path fill-rule="evenodd" clip-rule="evenodd" d="M2 12C2 6.48 6.47 2 11.99 2C17.52 2 22 6.48 22 12C22 17.52 17.52 22 11.99 22C6.47 22 2 17.52 2 12ZM15.97 8H18.92C17.96 6.35 16.43 5.07 14.59 4.44C15.19 5.55 15.65 6.75 15.97 8ZM12 4.04C12.83 5.24 13.48 6.57 13.91 8H10.09C10.52 6.57 11.17 5.24 12 4.04ZM4 12C4 12.69 4.1 13.36 4.26 14H7.64C7.56 13.34 7.5 12.68 7.5 12C7.5 11.32 7.56 10.66 7.64 10H4.26C4.1 10.64 4 11.31 4 12ZM5.08 16H8.03C8.35 17.25 8.81 18.45 9.41 19.56C7.57 18.93 6.04 17.66 5.08 16ZM5.08 8H8.03C8.35 6.75 8.81 5.55 9.41 4.44C7.57 5.07 6.04 6.34 5.08 8ZM12 19.96C11.17 18.76 10.52 17.43 10.09 16H13.91C13.48 17.43 12.83 18.76 12 19.96ZM9.5 12C9.5 12.68 9.57 13.34 9.66 14H14.34C14.43 13.34 14.5 12.68 14.5 12C14.5 11.32 14.43 10.65 14.34 10H9.66C9.57 10.65 9.5 11.32 9.5 12ZM14.59 19.56C15.19 18.45 15.65 17.25 15.97 16H18.92C17.96 17.65 16.43 18.93 14.59 19.56ZM16.5 12C16.5 12.68 16.44 13.34 16.36 14H19.74C19.9 13.36 20 12.69 20 12C20 11.31 19.9 10.64 19.74 10H16.36C16.44 10.66 16.5 11.32 16.5 12Z" fill="#F3EFEC" />
+		</svg>
+		<select name="color" id="color_filter" class="vida-select-filter">
 			<option value="all">All</option>
 			<option value="blue">Blue</option>
 			<option value="red">Red</option>
 			<option value="grey">Grey</option>
 			<option value="green">Green</option>
 		</select>
-		<select name="size" id="size_filter">
+		<label>Size:</label>
+		<select name="size" id="size_filter" class="vida-select-filter">
 			<option value="all">All</option>
 			<option value="small">Small</option>
 			<option value="medium">Medium</option>
 			<option value="large">Large</option>
 		</select>
-		<input type="submit" value="Find">
+		<input type="submit" value="Find" id="vida-shop-submitButton" class="vida-button-outline">
+		<input type="reset" value="Reset" id="vida-shop-resetButton">
 	</form>
 </div>
